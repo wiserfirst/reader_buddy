6d534de5ad2ccc898ca4f64359c86d4034f2a99d
Wu Qing <wiserfirst@gmail.com>
2018-03-18 19:18:06 +1100

    get ready for heroku

--- apps/cauldron/config/prod.exs
+++ apps/cauldron/config/prod.exs
@@ -15,8 +15,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :cauldron, CauldronWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "vast-beyond-12206.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -57,8 +58,3 @@ config :logger, level: :info
 # start per endpoint:
 #
 #     config :cauldron, CauldronWeb.Endpoint, server: true
-#
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
